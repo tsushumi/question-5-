7402749df6c7b998da744530cfbbe5857c8bb074
tsushumi <94306145+tsushumi@users.noreply.github.com>
2023-04-30 14:34:09 +0300

    Add files via upload

---
+++ question5/Process.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Process
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ResultLabel = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(92, 66)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(595, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'ResultLabel
+        '
+        Me.ResultLabel.AutoSize = True
+        Me.ResultLabel.Location = New System.Drawing.Point(12, 74)
+        Me.ResultLabel.Name = "ResultLabel"
+        Me.ResultLabel.Size = New System.Drawing.Size(39, 15)
+        Me.ResultLabel.TabIndex = 1
+        Me.ResultLabel.Text = "Result"
+        '
+        'Process
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ResultLabel)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Name = "Process"
+        Me.Text = "Process"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ResultLabel As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ question5/Process.vb
@@ -0,0 +1,21 @@
+﻿Public Class Process
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+        ProgressBar1.Location = New System.Drawing.Point(56, 72)
+        ProgressBar1.Size = New Size(168, 23)
+        ProgressBar1.TabIndex = 2
+        ProgressBar1.Value = 0
+    End Sub
+
+    Private Sub Process_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        ResultLabel.Visible = True
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ProgressBar1.Value <= ProgressBar1.Maximum - 1 Then
+            ProgressBar1.Value += 1
+            ResultLabel.Text = Int(ProgressBar1.Value * 100 / ProgressBar1.Maximum)
+            ResultLabel.Refresh()
+        Else Timer1.Enabled = False
+        End If
+    End Sub
+End Class
